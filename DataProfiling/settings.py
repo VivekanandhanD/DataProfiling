e19b324e17bc1ce2724bfea2ab738c3e2dd13b75
Vivekanandhan D <dvivekanandhan@hotmail.com>
2022-11-17 11:51:35 +0530

    Update settings.py

--- DataProfiling/settings.py
+++ DataProfiling/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kjrte=y75dj!hl=g@m*l!en)b84p9)$_9iro10n=v@yx8son9@'
+SECRET_KEY = 'upha(tegxlpezlotl_@bv^z&i4v-!05-$=9vxuc_rngx_^19w6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
